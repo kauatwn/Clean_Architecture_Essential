c5acb94e79273094fc12de1fe17fc5e98be90fb2
kauatwn <kaualopescontato@gmail.com>
2024-08-16 18:12:29 -0300

    Criando entidades de domínio

---
+++ src/Domain/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public sealed class Category : BaseEntity
+    {
+        public IReadOnlyCollection<Product> Products { get; } = [];
+
+        public Category(string name) : base(name) { }
+    }
+}
---
+++ src/Domain/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public sealed class Product : BaseEntity
+    {
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+        public string? ImageUrl { get; private set; }
+        public int CategoryId { get; private set; }
+
+        public Category? Category { get; }
+
+        public Product(string name, string description, decimal price, int stock, int categoryId, string? imageUrl = null) : base(name)
+        {
+            Description = description;
+            Price = price;
+            Stock = stock;
+            ImageUrl = imageUrl;
+            CategoryId = categoryId;
+        }
+    }
+}
