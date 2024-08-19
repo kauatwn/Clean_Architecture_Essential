0b03e4abe12847d7646ff8a6c818b0dd3e0aa8db
kauatwn <kaualopescontato@gmail.com>
2024-08-19 19:38:24 -0300

    Criando Dto Response para Category

---
+++ src/Application/Dtos/Responses/Category/CreateCategoryResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace Application.Dtos.Responses.Category
+{
+    public sealed record CreateCategoryResponse
+    {
+        public int Id { get; init; }
+        public string Name { get; init; }
+        public string? ImageUrl { get; init; }
+
+        public CreateCategoryResponse(int id, string name, string? imageUrl = null)
+        {
+            Id = id;
+            Name = name;
+            ImageUrl = imageUrl;
+        }
+    }
+}
