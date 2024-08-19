01cd11899f0e23b46ebc489ef069462495e85992
kauatwn <kaualopescontato@gmail.com>
2024-08-18 21:22:38 -0300

    Definindo ImageUrl no construtor da entidade base

--- src/Domain/Common/BaseEntity.cs
+++ src/Domain/Common/BaseEntity.cs
@@ -4,11 +4,13 @@
     {
         public int Id { get; protected set; }
         public string Name { get; protected set; }
+        public string? ImageUrl { get; protected set; }
         public DateTime CreatedOn { get; protected set; } = DateTime.UtcNow;
 
-        protected BaseEntity(string name)
+        protected BaseEntity(string name, string? imageUrl)
         {
             Name = name;
+            ImageUrl = imageUrl;
         }
     }
 }
