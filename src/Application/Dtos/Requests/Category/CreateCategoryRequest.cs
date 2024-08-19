ec3df0fba4fd63fc5319109a2489bf453630043a
kauatwn <kaualopescontato@gmail.com>
2024-08-19 19:37:58 -0300

    Criando Dto Request para Category

---
+++ src/Application/Dtos/Requests/Category/CreateCategoryRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Dtos.Requests.Category
+{
+    public sealed record CreateCategoryRequest
+    {
+        public string Name { get; init; }
+        public string? ImageUrl { get; init; }
+
+        public CreateCategoryRequest(string name, string? imageUrl = null)
+        {
+            Name = name;
+            ImageUrl = imageUrl;
+        }
+    }
+}
