0f258559e6e015daffbedf6cc1b8de5bff9a4ac9
kauatwn <kaualopescontato@gmail.com>
2024-08-20 20:02:42 -0300

    Criando abstração ICategoryMapper

---
+++ src/Application/Abstractions/Mappings/ICategoryMapper.cs
@@ -0,0 +1,11 @@
+﻿using Application.Dtos.Requests.Category;
+using Application.Dtos.Responses.Category;
+
+namespace Application.Abstractions.Mappings
+{
+    public interface ICategoryMapper
+    {
+        Domain.Entities.Category RequestToDomain(CreateCategoryRequest request);
+        CreateCategoryResponse DomainToResponse(Domain.Entities.Category category);
+    }
+}
