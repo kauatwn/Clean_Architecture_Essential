80790822a933dcd26d883dc022c1293f1c25d88d
kauatwn <kaualopescontato@gmail.com>
2024-08-20 20:03:52 -0300

    Criando classe concreta CategoryMapper

---
+++ src/Application/Mappings/Category/CategoryMapper.cs
@@ -0,0 +1,19 @@
+﻿using Application.Abstractions.Mappings;
+using Application.Dtos.Requests.Category;
+using Application.Dtos.Responses.Category;
+
+namespace Application.Mappings.Category
+{
+    public class CategoryMapper : ICategoryMapper
+    {
+        public Domain.Entities.Category RequestToDomain(CreateCategoryRequest request)
+        {
+            return new Domain.Entities.Category(request.Name, request.ImageUrl);
+        }
+
+        public CreateCategoryResponse DomainToResponse(Domain.Entities.Category category)
+        {
+            return new CreateCategoryResponse(category.Id, category.Name, category.ImageUrl);
+        }
+    }
+}
