f61514caa67e82579fa26b8af709b852197c5e76
kauatwn <kaualopescontato@gmail.com>
2024-08-21 21:22:32 -0300

    Criando Dto Response para Product

---
+++ src/Application/Dtos/Responses/Product/CreateProductResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace Application.Dtos.Responses.Product
+{
+    public sealed record CreateProductResponse
+    {
+        public int Id { get; init; }
+        public string Name { get; init; }
+        public string Description { get; init; }
+        public string? ImageUrl { get; init; }
+        public int CategoryId { get; init; }
+
+        public CreateProductResponse(int id, string name, string description, int categoryId, string? imageUrl = null)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            ImageUrl = imageUrl;
+            CategoryId = categoryId;
+        }
+    }
+}
