55d8c13471175de1e13c39690ef9c92f440f5ff7
kauatwn <kaualopescontato@gmail.com>
2024-08-21 21:22:17 -0300

    Criando Dto Request para Product

---
+++ src/Application/Dtos/Requests/Product/CreateProductRequest.cs
@@ -0,0 +1,22 @@
+﻿namespace Application.Dtos.Requests.Product
+{
+    public sealed record CreateProductRequest
+    {
+        public string Name { get; init; }
+        public string Description { get; init; }
+        public decimal Price { get; init; }
+        public int Stock {  get; init; }
+        public string? ImageUrl { get; init; }
+        public int CategoryId { get; init; }
+
+        public CreateProductRequest(string name, string description, decimal price, int stock, int categoryId, string? imageUrl = null)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+            CategoryId = categoryId;
+            ImageUrl = imageUrl;
+        }
+    }
+}
