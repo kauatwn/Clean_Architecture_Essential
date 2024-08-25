f7b3c31a9eb8c19866b380c83ad7c203a298ed2a
kauatwn <kaualopescontato@gmail.com>
2024-08-24 21:12:36 -0300

    Criando interface ICreateCategoryUseCase

---
+++ src/Application/Abstractions/UseCases/Category/ICreateCategoryUseCase.cs
@@ -0,0 +1,10 @@
+﻿using Application.Dtos.Requests.Category;
+using Application.Dtos.Responses.Category;
+
+namespace Application.Abstractions.UseCases.Category
+{
+    public interface ICreateCategoryUseCase
+    {
+        Task<CreateCategoryResponse> ExecuteAsync(CreateCategoryRequest request);
+    }
+}
