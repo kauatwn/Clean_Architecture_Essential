91ef6bae8a23393757dd2bf4b88a0f2175ce8ee3
kauatwn <kaualopescontato@gmail.com>
2024-09-01 10:33:12 -0300

    Criando CategoryRepository

---
+++ src/Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+using Domain.Interfaces.Repositories.Category;
+using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class CategoryRepository : ICategoryReadOnlyRepository, ICategoryWriteOnlyRepository
+    {
+        private ApplicationDbContext DbContext { get; }
+
+        public CategoryRepository(ApplicationDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public async Task AddAsync(Category category)
+        {
+            await DbContext.Categories.AddAsync(category);
+        }
+
+        public async Task<bool> ExistsByNameAsync(string name)
+        {
+            return await DbContext.Categories.AnyAsync(c => c.Name == name);
+        }
+    }
+}
