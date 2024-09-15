712af318e8e4acde82fdefb401aaf06559c8abf9
kauatwn <kaualopescontato@gmail.com>
2024-09-15 12:11:26 -0300

    Criando validador para CreateCategoryRequest

---
+++ src/Application/UseCases/Category/Create/CreateCategoryValidator.cs
@@ -0,0 +1,21 @@
+﻿using Application.DTOs.Requests.Category;
+using Application.Shared.Constants;
+using Application.Shared.Resources;
+using FluentValidation;
+
+namespace Application.UseCases.Category.Create;
+
+public class CreateCategoryValidator : AbstractValidator<CreateCategoryRequest>
+{
+    public CreateCategoryValidator()
+    {
+        RuleFor(r => r.Name)
+            .NotEmpty()
+            .WithMessage(ResourceExceptionMessages.EmptyNameMessage)
+            .Length(FieldValidation.NameMinLength, FieldValidation.NameMaxLength)
+            .WithMessage(string.Format(
+                ResourceExceptionMessages.NameLengthMessage,
+                FieldValidation.NameMinLength,
+                FieldValidation.NameMaxLength));
+    }
+}
