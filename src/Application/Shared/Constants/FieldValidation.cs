771ef13977e944f9abf9d725bad341c70b716de7
kauatwn <kaualopescontato@gmail.com>
2024-09-15 12:05:42 -0300

    Criando constantes para campos de validação

---
+++ src/Application/Shared/Constants/FieldValidation.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Shared.Constants;
+
+public static class FieldValidation
+{
+    public const int NameMinLength = 3;
+    public const int NameMaxLength = 256;
+    public const int DescriptionMinLength = 20;
+    public const int DescriptionMaxLength = 256;
+}
