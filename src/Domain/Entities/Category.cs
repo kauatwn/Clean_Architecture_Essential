047f17fbe5a1c174cd317200ad4d18b9fb8480dd
kauatwn <kaualopescontato@gmail.com>
2024-10-01 20:04:49 -0300

    Ajuste em Category para refletir mudança feita em BaseEntity

--- src/Domain/Entities/Category.cs
+++ src/Domain/Entities/Category.cs
@@ -5,8 +5,12 @@ namespace Domain.Entities;
 public sealed class Category : BaseEntity
 {
     public ICollection<Product> Products { get; private set; } = [];
-    
-    public Category(string name) : base(name)
+
+    public Category(int id, string name, string description) : base(id, name, description)
+    {
+    }
+
+    public Category(string name, string description) : base(name, description)
     {
     }
 }
