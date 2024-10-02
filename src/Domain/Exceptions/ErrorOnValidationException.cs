817b2ee19c75d160da721c785791a90435a0b5fc
kauatwn <kaualopescontato@gmail.com>
2024-10-02 17:05:56 -0300

    Criando ErrorOnValidationException

---
+++ src/Domain/Exceptions/ErrorOnValidationException.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+using Domain.Exceptions.Base;
+
+namespace Domain.Exceptions;
+
+public sealed class ErrorOnValidationException(IList<string> errors)
+    : DomainException("The request was not processed due to one or more validation errors")
+{
+    public IList<string> Errors { get; } = errors;
+
+    public override int StatusCode => (int)HttpStatusCode.UnprocessableEntity;
+}
