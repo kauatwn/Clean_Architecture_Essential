8d8dcc9a95e0678a760d3c3e57a9b76a00e53319
kauatwn <kaualopescontato@gmail.com>
2024-10-03 16:13:53 -0300

    Criando teste unitário para validação de descrição

--- test/UnitTests/Application.UnitTests/UseCases/Category/Create/CreateCategoryValidatorTest.cs
+++ test/UnitTests/Application.UnitTests/UseCases/Category/Create/CreateCategoryValidatorTest.cs
@@ -18,6 +18,7 @@ public class CreateCategoryValidatorTest
         // Arrange
         var validName = Faker.Commerce.Department();
         var validDescription = Faker.Commerce.ProductDescription();
+        
         var request = new CreateCategoryRequest(validName, validDescription);
 
         // Act
@@ -34,6 +35,7 @@ public class CreateCategoryValidatorTest
     {
         // Arrange
         var validDescription = Faker.Commerce.ProductDescription();
+        
         var request = new CreateCategoryRequest(invalidName!, validDescription);
 
         // Act
@@ -50,6 +52,7 @@ public class CreateCategoryValidatorTest
         // Arrange
         var shortName = Faker.Lorem.Letter(FieldValidation.NameMinLength - 1);
         var validDescription = Faker.Commerce.ProductDescription();
+        
         var request = new CreateCategoryRequest(shortName, validDescription);
 
         // Act
@@ -69,6 +72,7 @@ public class CreateCategoryValidatorTest
         // Arrange
         var longName = Faker.Lorem.Letter(FieldValidation.NameMaxLength + 1);
         var validDescription = Faker.Commerce.ProductDescription();
+        
         var request = new CreateCategoryRequest(longName, validDescription);
 
         // Act
@@ -80,5 +84,79 @@ public class CreateCategoryValidatorTest
                 ResourceExceptionMessages.NameLengthMessage,
                 FieldValidation.NameMinLength,
                 FieldValidation.NameMaxLength));
+    }
+
+    [Fact]
+    public void ShouldNotHaveErrorWhenDescriptionIsValid()
+    {
+        // Arrange
+        var validName = Faker.Commerce.Department();
+        var validDescription = Faker.Commerce.ProductDescription();
+        
+        var request = new CreateCategoryRequest(validName, validDescription);
+
+        // Act
+        var result = Validator.TestValidate(request);
+
+        // Assert
+        result.ShouldNotHaveValidationErrorFor(r => r.Description);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public void ShouldHaveErrorWhenDescriptionIsNullOrEmpty(string? invalidDescription)
+    {
+        // Arrange
+        var validName = Faker.Commerce.Department();
+        
+        var request = new CreateCategoryRequest(validName, invalidDescription!);
+
+        // Act
+        var result = Validator.TestValidate(request);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(r => r.Description)
+            .WithErrorMessage(ResourceExceptionMessages.NullOrEmptyDescriptionMessage);
+    }
+
+    [Fact]
+    public void ShouldHaveErrorWhenDescriptionIsTooShort()
+    {
+        // Arrange
+        var validName = Faker.Commerce.Department();
+        var shortDescription = Faker.Lorem.Letter(FieldValidation.DescriptionMinLength - 1);
+        
+        var request = new CreateCategoryRequest(validName, shortDescription);
+
+        // Act
+        var result = Validator.TestValidate(request);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(r => r.Description)
+            .WithErrorMessage(string.Format(
+                ResourceExceptionMessages.DescriptionLengthMessage,
+                FieldValidation.DescriptionMinLength,
+                FieldValidation.DescriptionMaxLength));
+    }
+
+    [Fact]
+    public void ShouldHaveErrorWhenDescriptionIsTooLong()
+    {
+        // Arrange
+        var validName = Faker.Commerce.Department();
+        var longDescription = Faker.Lorem.Letter(FieldValidation.DescriptionMaxLength + 1);
+        
+        var request = new CreateCategoryRequest(validName, longDescription);
+
+        // Act
+        var result = Validator.TestValidate(request);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(r => r.Description)
+            .WithErrorMessage(string.Format(
+                ResourceExceptionMessages.DescriptionLengthMessage,
+                FieldValidation.DescriptionMinLength,
+                FieldValidation.DescriptionMaxLength));
     }
 }
