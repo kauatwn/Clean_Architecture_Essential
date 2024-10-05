2c793c7150eae562c1a56f7231a1994e66f1b0dd
kauatwn <kaualopescontato@gmail.com>
2024-10-05 09:31:43 -0300

    Implementado endpoint de criação de categorias

---
+++ src/API/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using Application.Abstractions.UseCases.Category;
+using Application.DTOs.Requests.Category;
+using Application.DTOs.Responses.Category;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class CategoriesController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType<CategoryResponse>(StatusCodes.Status201Created)]
+    [ProducesResponseType<ProblemDetails>(StatusCodes.Status422UnprocessableEntity)]
+    [ProducesResponseType<ProblemDetails>(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> Create(ICreateCategoryUseCase useCase, CreateCategoryRequest request)
+    {
+        var result = await useCase.ExecuteAsync(request);
+
+        return Created(string.Empty, result);
+    }
+}
